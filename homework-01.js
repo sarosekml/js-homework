3a659c2bfc9aa8d5e548b15185c7f8635a7ffbff
mikhail <sarosekml@gmail.com>
2026-05-08 18:11:57 +0300

    first homework

---
+++ homework-01.js
@@ -0,0 +1,26 @@
+/*
+Написать код, рассчитывающий дистанцию о объекта: 
+
+addressLat - адрес назначения lat (например: 10)
+addressLong - адрес назначения long
+positionLat - текущее положение пользователя lat
+positionLong - текущее положение пользователя long
+В результате на основе этих координат нужно посчитать расстояние от текущего месторасположения до назначения.
+*/
+
+//address
+let addressLat = 96; // ← Y2
+let addressLong = 64; // ← X2
+
+//user position
+let positionLat = 8; // ← Y1
+let positionLong = 16; // ← X1
+
+//distance
+//c = √(a² + b²)
+let catA = addressLong - positionLong; //a
+let catB = addressLat - positionLat; // b
+
+let distanceToAddress = Math.sqrt(catA ** 2 + catB ** 8); // c
+
+console.log(distanceToAddress); //answer
