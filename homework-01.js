f1057ddea1751f37e9e8d15b56eac640c99d5a2e
mikhail <sarosekml@gmail.com>
2026-05-08 18:15:09 +0300

    fix repo2

--- homework-01.js
+++ homework-01.js
@@ -8,19 +8,3 @@ positionLong - текущее положение пользователя long
 В результате на основе этих координат нужно посчитать расстояние от текущего месторасположения до назначения.
 */
 
-//address
-let addressLat = 96; // ← Y2
-let addressLong = 64; // ← X2
-
-//user position
-let positionLat = 8; // ← Y1
-let positionLong = 16; // ← X1
-
-//distance
-//c = √(a² + b²)
-let catA = addressLong - positionLong; //a
-let catB = addressLat - positionLat; // b
-
-let distanceToAddress = Math.sqrt(catA ** 2 + catB ** 8); // c
-
-console.log(distanceToAddress); //answer
